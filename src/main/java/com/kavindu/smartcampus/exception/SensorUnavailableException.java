f921e2abd3d35ba5391ea4e64ea1c00eadf805de
Your Name <you@example.com>
2026-04-24 02:51:16 +0530

    feat: initialize Smart Campus API project with JAX-RS configuration and custom exception hierarchy

---
+++ src/main/java/com/kavindu/smartcampus/exception/SensorUnavailableException.java
@@ -0,0 +1,7 @@
+package com.kavindu.smartcampus.exception;
+
+public class SensorUnavailableException extends RuntimeException {
+    public SensorUnavailableException(String message) {
+        super(message);
+    }
+}
